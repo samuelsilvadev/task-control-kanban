f47a3fa7e82571664f9f07b5a69405539b808d70
Samuel SIlva <samuelsilvawb@gmail.com>
2018-04-09 20:29:39 -0300

    chore : add dependencie firebase
    - installed dependencie in project
    - configured data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDlJJH67lJXvgGon7oxi7lG1VxnuBv-xg8',
+    authDomain: 'task-control-kanban.firebaseapp.com',
+    databaseURL: 'https://task-control-kanban.firebaseio.com',
+    projectId: 'task-control-kanban',
+    storageBucket: 'task-control-kanban.appspot.com',
+    messagingSenderId: '440784810409'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDlJJH67lJXvgGon7oxi7lG1VxnuBv-xg8',
+    authDomain: 'task-control-kanban.firebaseapp.com',
+    databaseURL: 'https://task-control-kanban.firebaseio.com',
+    projectId: 'task-control-kanban',
+    storageBucket: 'task-control-kanban.appspot.com',
+    messagingSenderId: '440784810409'
+  }
 };
